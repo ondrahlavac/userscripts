746366d8608884d9d6feccf8a0005581dece0bd2
Ondra Hlaváč <ondra@hlavac.cz>
2023-12-21 15:08:22 +0100

    feat: add new version of the WEDOS payment page userscript and mention it in README

--- README.md
+++ README.md
@@ -9,3 +9,24 @@ keep tinkering with.
 If you are using GreaseMonkey, all you have to do is choose a userscript you
 like, view it in RAW and you will be prompted to install it. It does not get
 easier than that.
+
+
+
+# List of my user scripts
+
+## Wedos - payment page favourites
+
+### Objective
+
+When a user comes to the payment page, select his favourite payment method and hide all other methods to hide the clutter.
+
+### How to do it
+- On page load, find all the rows with payment methods.
+- For each of the rows, determine the name of the method.
+  - If the name of the method is the same as the favourite one of the user, select the input radio in it.
+  - Otherwise hide the row.
+
+### Future enhancements
+- add a button to show all hidden methods
+- add a star emoji button next to name of the method, to let the user set it as his favourite
+- save the key of the payment method in the keyvalue store of your monkey flavour extension
---
+++ wedos-payment-page-favourites.user.js
@@ -0,0 +1,55 @@
+// ==UserScript==
+// @name        WEDOS - Streamlining payment page
+// @version     2.0
+// @author      Ondra Hlaváč <ondra@hlavac.cz>
+// @description Preselects yuor favourite payment method and hides the rest.
+// @match       https://client.wedos.com/pay/order*
+// @grant       GM.setValue
+// @grant       GM.getValue
+// @license     MIT
+// @run-at      document-end
+// @homepageURL https://github.com/ondrahlavac/userscripts
+// @updateURL   https://raw.githubusercontent.com/ondrahlavac/userscripts/master/wedos-payment-page-favourites.user.js
+// @downloadURL https://raw.githubusercontent.com/ondrahlavac/userscripts/master/wedos-payment-page-favourites.user.js
+// @supportURL  https://github.com/ondrahlavac/userscripts/issues
+// ==/UserScript==
+
+const availableMethods = Array()
+
+const pollAvailableMethods = () => {
+  let methodsRows = document.querySelectorAll('#methods_list table.orderpay_method_tbl')
+  for (var i=0; i < methodsRows.length; i++) {
+    let methodRow, methodId, methodRadioButton
+    methodRow = methodsRows[i]
+    try {
+      methodId = methodRow.querySelector('td.frm_title label').getAttribute('for')
+      methodRadioButton = methodRow.querySelector(`input#${methodId}`)
+    } catch (ex) {
+      // ignoring DOMException in case of nonstandard ids
+      // like 'frm_input_method_bank:CZK' which don't play nice with querySelector()
+      // console.warn(ex);
+    }
+    availableMethods.push({
+      'id': methodId,
+      'rowElement': methodRow,
+      'radioButton': methodRadioButton,
+    })
+  }
+}
+
+const simplifyAndPreselectPaymentMethods = (fav) => {
+  for (var i=0; i < availableMethods.length; i++) {
+    const method = availableMethods[i]
+    if (method.id == fav) {
+      method.radioButton.click()
+    } else {
+      method.rowElement.style.display = 'none'
+    }
+  }
+}
+
+window.addEventListener('load', async function () {
+  pollAvailableMethods();
+  let favouriteMethodId = await GM.getValue('favourite', 'frm_input_method_cc')
+  simplifyAndPreselectPaymentMethods(favouriteMethodId);
+}, false);
